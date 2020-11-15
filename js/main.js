3367b3a88de072005f737e89c57c51b036d000ea
Artem <temapro04@gmail.com>
2020-11-15 17:03:23 +0200

    Vikings

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 $(function(){
-    $('.photo__slider').slick({
-        prevArrow: '<button type="button" class="slick-btn slick-left"><img src="images/arrow-left.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-btn slick-right"><img src="images/arrow-right.svg" alt=""></button>',
-        autoplay: true, 
+
+    //делает чтобы меню выежало в низ
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').toggleClass('menu__list--active')
     });
 
-    $('.menu__btn').on('click', function(){
-        $('.menu__list').slideToggle();
+    $('.heroes__slider-img').slick({
+        prevArrow: '<button type="button" class="slider-btn slider-btn__left"><img src="images/arrow-left.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slider-btn slider-btn__right"><img src="images/arrow-right.png" alt=""></button>',
+        slidesToShow: 1,
+        sliderToScroll: 1,
+        asNavFor: '.heroes__slider-text',
+        responsive: [
+            {
+                breakpoint: 639,
+                settings:{
+                    arrows: false,
+                }
+            }
+        ]
+    });
+
+    $('.heroes__slider-text').slick({
+        slidesToShow: 1,
+        sliderToScroll: 1,
+        asNavFor: '.heroes__slider-img',
+        fade: true,
+        arrows: false
     });
 });
